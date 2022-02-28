dba05a35de2603cad977c3e2e4c126a8df45c442
princewhyte dabotubo <princewhyte02@gmail.com>
2022-02-28 17:56:16 +0100

    assessment page

---
+++ pages/assessment.tsx
@@ -0,0 +1,421 @@
+import react from "react"
+
+const Assessment = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen h-full text-white bg-blue-400">
+      <div>
+        <h1 className="text-xl font-bold text-center ">Assessment Checks</h1>
+        <form className="p-4 text-black bg-white">
+          <details>
+            <summary>Business Case Study</summary>
+            <main className="flex flex-col space-y-2">
+              <section>
+                <h3>Evaluate & Frame Oppportunities</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Deliver Commercial Value</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Project Risk Management</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+            </main>
+          </details>
+          <details>
+            <summary>Competitive & Affordable Scope</summary>
+            <main className="flex flex-col space-y-2">
+              <section>
+                <h3>Select & Optimise Capital Efficient Project Concept</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Leverage Portfolio Benefit</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Project/Portfolio Benchmarking</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Manage Design Engineering</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+            </main>
+          </details>
+          <details>
+            <summary>Lead Efficient Execution</summary>
+            <main className="flex flex-col space-y-2">
+              <section>
+                <h3>Develop Project Execution Strategies & Plans </h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Contract & Contractor Management</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Set up & Lead Project Teams</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Work Efficiently With Stakeholders</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Manage Project Complexities</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+            </main>
+          </details>
+          <details>
+            <summary>Manage Efficient Execution</summary>
+            <main className="flex flex-col space-y-2">
+              <section>
+                <h3>Develop Project Execution Strategies & Plans </h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Contract & Contractor Management</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Set up & Lead Project Teams</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Work Efficiently With Stakeholders</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+              <section>
+                <h3>Manage Project Complexities</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+            </main>
+          </details>
+          <details>
+            <summary>Interface Management</summary>
+            <main className="flex flex-col space-y-2">
+              <section>
+                <h3>Lead Interface Management</h3>
+                <div className="flex items-center space-x-2">
+                  <div className="flex-1 ">
+                    <label className="block">
+                      <input type="radio" /> Awareness = Can describe in basic terms
+                    </label>
+
+                    <label className="block">
+                      <input type="radio" /> Knowledge = Awareness plus, can explain and have an informed debate and
+                      participation
+                    </label>
+                    <label className="block">
+                      <input type="radio" /> Skill = Knowledge plus, can do the work
+                    </label>
+                  </div>
+                  <div>
+                    <input type={"file"} placeholder="File:Attach file evidence" />
+                  </div>
+                </div>
+              </section>
+            </main>
+          </details>
+          <div className="flex justify-center mt-6">
+            <button className="px-4 py-2 text-white bg-blue-600 ">Continue</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Assessment
